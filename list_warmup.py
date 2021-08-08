6c697c9597a0cdfdaa42dfec79d7561395352d46
89204 <yanjun.ye@my.jcu.edu.au>
2021-08-08 16:32:39 +0800

    Initial commit

---
+++ list_warmup.py
@@ -0,0 +1,36 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# What values do the following expressions have?
+# (solutions not provided; figure it out, then run with print or in console to test)
+print(numbers[0])
+
+print(numbers[-1])
+
+print(numbers[3])
+
+print(numbers[:-1])
+
+print(numbers[3:4])
+
+5 in numbers
+
+7 in numbers
+
+"3" in numbers
+
+numbers + [6, 5, 3]
+print(numbers)
+
+# Write Python expressions (in your Python file) to achieve the following:
+
+# Change the first element of numbers to "ten"
+numbers[0] = "ten"
+print(numbers)
+# Change the last element of numbers to 1
+numbers[-1] = 1
+print(numbers)
+# Get all the elements from numbers except the first two
+numbers[2:]
+
+# Check if 9 is an element of numbers
+9 in numbers
